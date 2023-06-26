aa4e33f32318197beabab79500c071c9a38a5aee
Madeleine Lorås <6739072+Madelelo@users.noreply.github.com>
2023-06-26 16:30:11 +0200

    Oppdatering med lyder

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ def on_forever():
     global tid
     tid = 5
     while tid > 0 and input.button_is_pressed(Button.AB):
-        # #while tid < 0 :
+        # while tid < 0:
         basic.show_number(tid)
         tid = tid - 1
         basic.pause(100)
+        music.play(music.tone_playable(3000, music.beat(BeatFraction.WHOLE)),
+            music.PlaybackMode.UNTIL_DONE)
     if tid == 0:
+        music.play(music.tone_playable(3000, music.beat(BeatFraction.WHOLE)),
+            music.PlaybackMode.IN_BACKGROUND)
         radio.send_number(1)
         basic.show_icon(IconNames.HAPPY)
         basic.pause(2000)
--- main.ts
+++ main.ts
@@ -1,20 +1,23 @@
 let tid = 0
 radio.setGroup(12)
 basic.showIcon(IconNames.Yes)
-basic.forever(function on_forever() {
-    
+music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.UntilDone)
+basic.showString("Sender")
+basic.showIcon(IconNames.Yes)
+basic.forever(function () {
     tid = 5
     while (tid > 0 && input.buttonIsPressed(Button.AB)) {
-        //  #while tid < 0 :
+        // while tid < 0:
         basic.showNumber(tid)
         tid = tid - 1
         basic.pause(100)
+        music.play(music.tonePlayable(3000, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
     }
     if (tid == 0) {
+        music.play(music.tonePlayable(3000, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
         radio.sendNumber(1)
         basic.showIcon(IconNames.Happy)
         basic.pause(2000)
         basic.clearScreen()
     }
-    
 })
